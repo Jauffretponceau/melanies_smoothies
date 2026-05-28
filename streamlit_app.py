79d7f29bc211f8cbc41eef4247ac12c0eed715d3
Jauffretponceau <jauffret.ponceau@kpconsulting.fr>
2026-05-28 17:07:34 +0200

    Fetch nutrition info for selected fruits from API
    Updated API call to fetch nutrition information for each selected fruit dynamically.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
 
-# API
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie ! :cup_with_straw:")
@@ -34,8 +33,11 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+      
+        # API
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-    #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
