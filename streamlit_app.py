d756c8f1a1107724129caefdb285badabfffc17a
Jauffretponceau <jauffret.ponceau@kpconsulting.fr>
2026-05-28 17:00:29 +0200

    Display smoothie data as dataframe instead of text
    Replace text output with dataframe display for smoothie data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie ! :cup_with_straw:")
@@ -46,3 +44,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+ name_on_order, icon="✅")
 
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+# st.text(smoothiefroot_response.json())
+
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_with=True)
